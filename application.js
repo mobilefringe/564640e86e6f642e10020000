d626578e9f959790625f8b0022b9cafddc58733d
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2015-11-16 12:38:59 -0500

    ShopCentral 1.0  /application.js

--- application.js
+++ application.js
@@ -1,22 +1,24 @@
 /*Created 2015-02-28  by RKS*/
 
-$('#newsletter_form').submit(function(e){
-   e.preventDefault();
-   if($('#agree_terms').prop('checked') == false){
-        $('#agree_terms').focus();
-        alert("Please agree to receieve newsletters from Shop Central.")
-   }
-   else{
-       $.getJSON(
-            this.action + "?callback=?",
-            $(this).serialize(),
-            function (data) {
-                if (data.Status === 400) {
-                    alert("Please try again later.");
-                } else { // 200
-                    $("#subscription_confirmed").fadeIn();
-                }
-        });
-   }
-
+$(document).ready(function(e){
+    $('#newsletter_form').submit(function(e){
+       e.preventDefault();
+       if($('#agree_terms').prop('checked') == false){
+            $('#agree_terms').focus();
+            alert("Please agree to receieve newsletters from Shop Central.")
+       }
+       else{
+           $.getJSON(
+                this.action + "?callback=?",
+                $(this).serialize(),
+                function (data) {
+                    if (data.Status === 400) {
+                        alert("Please try again later.");
+                    } else { // 200
+                        $("#subscription_confirmed").fadeIn();
+                    }
+            });
+       }
+    
+    });
 });
