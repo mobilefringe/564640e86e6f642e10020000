654c0ac9fa91796e5643bb873ac751626775bf1f
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2015-11-16 12:33:51 -0500

    ShopCentral 1.0  /application.js

--- application.js
+++ application.js
@@ -2,5 +2,15 @@
 
 $('#newsletter_form').submit(function(e){
    e.preventDefault();
-   
+   $.getJSON(
+        this.action + "?callback=?",
+        $(this).serialize(),
+        function (data) {
+            if (data.Status === 400) {
+                alert("Please try again later.");
+            } else { // 200
+                $("#success_subscribe").fadeIn();
+            }
+    });
+
 });
