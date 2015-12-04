e00085d7d52910d6bc887f479f502c1700561af0
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2015-12-04 14:34:24 -0500

    ShopCentral 1.0  /application.js

--- application.js
+++ application.js
@@ -20,4 +20,21 @@ $(document).ready(function(e){
             });
        }
     });
+    
+    $('#email_form').submit(function(e){
+        e.preventDefault();
+        data = {};
+        data.send_to = "support@mobilefringe.com";
+        data.subject = "Get in touch - Mobile Fringe form";
+        data.body = {"email" : $('#yourEmail').val(), "name" : $('#yourName').val(), "message" : $('#yourMessage').val()};
+        $.post('http://mobilefringe.mallmaverick.com/send_contact_email', data, function(data, textStatus, jqXHR){
+            if(textStatus == "success"){
+                $('#email_sent').fadeIn();
+            }
+            else{
+                alert("Error sending email. Please try again later.");
+            }
+        });
+    });
 });
+
