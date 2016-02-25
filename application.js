08a5c06d11d322787b79bc552b10aa4e85726cd1
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-02-25 12:43:57 -0500

    ShopCentral 1.01  /application.js

---
+++ application.js
@@ -0,0 +1,50 @@
+/*Created 2015-02-28  by RKS*/
+
+$(document).ready(function(e){
+    $('#newsletter_form').submit(function(e){
+       e.preventDefault();
+       if($('#agree_terms').prop('checked') == false){
+            $('#agree_terms').focus();
+            alert("Please agree to receieve newsletters from Shop Central.")
+       }
+       else{
+           $.getJSON(
+                this.action + "?callback=?",
+                $(this).serialize(),
+                function (data) {
+                    if (data.Status === 400) {
+                        alert("Please try again later.");
+                    } else { // 200
+                        $("#subscription_confirmed").fadeIn();
+                    }
+            });
+       }
+    });
+    
+    $('#contact_form').submit(function(e){
+        e.preventDefault();
+        data = {};
+        data.send_to = "shopcentral@mobilefringe.com";
+        data.subject = "Get in touch - Shop Central";
+        var message = $('#message').val();
+        data.body = {"Email" : $('#email').val(), "Name" : $('#name').val(), "Feedback type: ": $('#feeback_type').val(), "Message" : message};
+        $.post('http://mobilefringe.mallmaverick.com/send_contact_email', data, function(data, textStatus, jqXHR){
+            if(textStatus == "success"){
+                $('#email_sent').fadeIn();
+                $('#contact_form').trigger('reset');
+            }
+            else{
+                alert("Error sending email. Please try again later.");
+            }
+        });
+    });
+    
+    function awesomeness(){
+        $(window).scroll(function(){
+            
+        });
+    }
+});
+
+
+
