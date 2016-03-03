d794c39ceaf75bd6a493025090381c30360674d9
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-03-03 17:03:41 -0500

    ShopCentral 1.01  /application.js

--- application.js
+++ application.js
@@ -60,7 +60,7 @@ $(document).ready(function(e){
     
     function awesomeness(){
     $(window).scroll(function(){
-        if($(document).scrollTop() >= $('.how_it_works').position().top && $(document).scrollTop() < $('.how_it_works').height()){
+        if($(document).scrollTop() >= $('.how_it_works').position().top ){
             $('.image_cover').css("top", "166px");
             $('.image_cover').addClass("fixed_image");
             
