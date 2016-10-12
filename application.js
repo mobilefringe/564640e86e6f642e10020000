d2ac0f46020d050932a127415feed4a63f0cd98c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-12 10:38:39 -0400

    ShopCentral 1.02  /application.js

---
+++ application.js
@@ -0,0 +1,89 @@
+/*Created 2015-02-28  by RKS*/
+
+$(document).ready(function(e){
+    $('#newsletter_form').submit(function(e){
+       e.preventDefault();
+       if($('#agree_terms').prop('checked') == false){
+            $('#agree_terms').focus();
+            alert("Please agree to receieve newsletters from Shop Central.")
+       }
+       else{
+           $.getJSON(
+                this.action + "?callback=?",
+                $(this).serialize(),
+                function (data) {
+                    if (data.Status === 400) {
+                        alert("Please try again later.");
+                    } else { // 200
+                        $("#subscription_confirmed").fadeIn();
+                    }
+            });
+       }
+    });
+    
+    $('#toggle_menu').click(function(){
+        $(this).toggleClass("fa-chevron-down fa-chevron-up");
+        if ($('.mobile_menu').hasClass("open")){
+            $('.mobile_menu').slideUp();    
+            $('.mobile_menu').removeClass("open");
+        }
+        else{
+            $('.mobile_menu').slideDown();
+            $('.mobile_menu').addClass("open");
+        }
+        
+    })
+    
+    $('#contact_form').submit(function(e){
+        e.preventDefault();
+        data = {};
+        data.send_to = "shopcentral@mobilefringe.com";
+        data.subject = "Get in touch - Shop Central";
+        var message = $('#message').val();
+        data.body = {"Email" : $('#email').val(), "Name" : $('#name').val(), "Feedback type: ": $('#feeback_type').val(), "Message" : message};
+        $.post('//mobilefringe.mallmaverick.com/send_contact_email', data, function(data, textStatus, jqXHR){
+            if(textStatus == "success"){
+                $('#email_sent').fadeIn();
+                $('#contact_form').trigger('reset');
+            }
+            else{
+                alert("Error sending email. Please try again later.");
+            }
+        });
+    });
+    
+    
+});
+
+
+    
+    function awesomeness(){
+    $(window).scroll(function(){
+        if($(document).scrollTop() >= $('.how_it_works').position().top ){
+            $('.image_cover').css("top", "166px");
+            $('.image_cover').addClass("fixed_image");
+            $('#heading_works').addClass("fixed_text");
+            
+        } 
+        else{
+            $('.image_cover').removeClass("fixed_image");
+            $('#heading_works').removeClass("fixed_text");
+        }
+    });
+}
+
+function renderStoreList(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html);
+    $.each( collection , function( key, val ) {
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+    });
+    $(container).show();
+    $(container).append(item_rendered.join(''));
+}
+
+
+
